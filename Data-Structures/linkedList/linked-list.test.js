7e554de34092a1f57bba332a3e7d791cb870c398
PaulDepew <Paul@lostamongpines.com>
2020-06-15 21:56:33 -0700

    class-06

--- Data-Structures/linkedList/linked-list.test.js
+++ Data-Structures/linkedList/linked-list.test.js
@@ -61,4 +61,68 @@ describe('Testing that we can return a list of the notes found', () => {
   });
 });
 
+describe('Testing that we can add a note to the end of a list', () => {
+  it('should return add a note to the end of the list', () => {
+    let list = new linkedList.LinkedList();
+    list.insert('George');
+    list.insert('Hi Mom');
+    list.append('hi Dad');
+
+    expect(list.head.value).toBe('hi Dad');
+  });
+});
+
+describe('Testing that we can add multiple nodes to the end of the list', () => {
+  it('should return the last two notes added to the list', () => {
+    let list = new linkedList.LinkedList();
+    list.insert('George');
+    list.insert('Hi Mom');
+    list.append('hi Dad');
+    list.append('ME?');
+
+    expect(list.head.value).toBe('ME?');
+    expect(list.head.next.value).toBe('hi Dad');
+  });
+});
+
+// describe('Testing that we can insert a node before the first node', () => {
+//   it('should return the note placed infront of the previous', () => {
+//     let list = new linkedList.LinkedList();
+//     list.insert('George');
+//     list.insert('Hi Mom');
+//     list.insert('hi Dad');
+//     list.insertBefore('hi Dad', 'test');
+//     console.log(list.head);
+
+//     expect(list.head.value).toBe('test');
+    
+//   });
+// });
+
+describe('Testing that we can insert a node in the middle of the list', () => {
+  it('should return the note in the middle of the lsit', () => {
+    let list = new linkedList.LinkedList();
+    list.insert('George');
+    list.insert('Hi Mom');
+    list.insert('hi Dad');
+    list.insertAfter('Hi Mom', 'test');
+    // console.log(list.head.next.next.value);
+
+    expect(list.head.next.next.value).toBe('test');
+  });
+});
+
+// describe('Testing that we can insert a node at the end of the list', () => {
+//   it('should return the note at the end of the list', () => {
+//     let list = new linkedList.LinkedList();
+//     list.insert('George');
+//     list.insert('Hi Mom');
+//     list.insert('hi Dad');
+//     list.insertAfter('George', 'test');
+//     console.log(list.head.next.next);
+
+//     expect(list.head.next.next.next.value).toEqual('test');
+    
+//   });
+// });
 
